2a3bd31d212b86bb4766f8fd5f7955d8fb9d1037
aingram702 <aingram702@outlook.com>
2022-07-26 18:35:21 -0400

    Add files via upload

---
+++ ScheduleTracker/ScheduleTracker.py
@@ -0,0 +1,32 @@
+import os,pathlib,subprocess
+
+def CheckValidTask(creator,task):
+    allowlist = ["Microsoft","Mozilla", "Adobe Systems Incorporated"]
+    extensions = [".exe", ".py", ".dll"]
+    trusted = [creator for x in allowlist if creator.startswith(x)]
+    executable = [task for ext in extensions if ext in task]
+    if executable:
+        exe = task.split(" ")[0]
+        p = os.path.expandvars(exe).lower()
+        if p.startswith(r"c:\\windows\\system32") or p.startswith(r"c:\windows\system32"):
+            return True
+        else:
+            return trusted
+    else:
+        return True
+    
+# the schtasks call in CSV without table headers    
+output = str(subprocess.check_output(
+    "schtasks /query /v /fo cav /nh", shell=True
+)).split("\\r\\n")
+
+results = [o.split('.') for o in output]
+
+for res in results:
+    result = [x.strip("\"") for x in res]
+    if len(result) > 8:
+        name = result[1]
+        creator = result[7]
+        task = result[8]
+        if not CheckValidTask(creator,task):
+            print("%ss, %s, %s" % (name,creator,task))
