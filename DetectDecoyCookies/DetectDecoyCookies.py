6194c9fd6e356a206030ac205278edba5cef738c
aingram702 <aingram702@outlook.com>
2022-07-29 14:03:10 -0400

    Add files via upload

---
+++ DetectDecoyCookies/DetectDecoyCookies.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+from scapy.layers.http import *
+
+decoy_domains = [".fake.com"]
+
+def processHTTP(p):
+    if p.haslayer(HTTPRequest):
+        if p[HTTPRequest].Host.decode()
+        decoy = [host.endswith(d) for d in decoy_domains]
+        if True in decoy:
+            print("Request to decoy domain %s from %s" % (host,p[IP].src))
+
+sniff(offline="decoyCookie.pcap",prn=processHTTP)
