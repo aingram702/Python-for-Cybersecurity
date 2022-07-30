290af4add04910147a99c27bb45296074624f85b
aingram702 <aingram702@outlook.com>
2022-07-30 05:49:16 -0400

    Delete TerminateAntivirus directory

--- TerminateAntivirus/TerminateAntivirus.py
+++
@@ -1,9 +0,0 @@
-import psutil,os,signal
-
-av_list = ["notepad"]
-
-# Find and Kill Processes
-for process in psutil.process_iter():
-    for name in av_list:
-        if name in process.name():
-            os.kill(process.pid,signal.SIGTERM)
